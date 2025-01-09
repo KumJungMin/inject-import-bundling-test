a9aea7ad89c4239059d1f2b0b2973bb73a24c657
KumJungMin <rmawjdals@naver.com>
2025-01-09 22:25:15 +0900

    feat: add 100 test components and 50 util function

---
+++ src/components/Test66.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div>12312323</div>
+    <div>12312323</div>
+    <div>12312323</div>
+    <div>12312323</div>
+    <div>12312323</div>
+    <div>12312323</div>
+    <div>12312323</div>
+    <div>12312323</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { util1, util50, util13 } from '../utils'
+
+util1()
+util50()
+util13()
+</script>
